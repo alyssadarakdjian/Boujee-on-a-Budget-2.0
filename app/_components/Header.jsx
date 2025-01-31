262242c6c6a9a0fc248441e59bc80276e7b7d548
Ross <rharrison@oakland.edu>
2025-01-31 12:17:05 -0500

    Initial commit - adding existing project

---
+++ app/_components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div>
+
+     <img src="/logo.svg" alt="logo" width="160" height="100" />
+
+
+    </div>
+  )
+}
+
+export default Header
