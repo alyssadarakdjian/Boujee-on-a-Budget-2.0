5f2b0dfb07b0c40683c30cf4f026232b5f823099
Ross <rharrison@oakland.edu>
2025-02-05 17:26:31 -0500

    changed landing screen

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 function Header() {
   return (
-    <div>
+    <div className='p-5 flex justify-between items-center border shadow-md'>
 
      <img src="/logo.svg" alt="logo" width="160" height="100" />
 
+    <Button>Get Started</Button>
 
     </div>
   )
