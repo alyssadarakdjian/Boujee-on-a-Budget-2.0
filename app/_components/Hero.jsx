e940c8652266634803ca81879266a3810eff17bc
Ross <rharrison@oakland.edu>
2025-02-05 19:32:46 -0500

    sign in and sign up are createde

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Image from "next/image";
+
 
 function Hero() {
   return (
-    <section className="bg-gray-900 text-white">
+    <section className="bg-gray-900 text-white flex items-center flex-col" >
   <div className="mx-auto max-w-screen-xl px-4 py-32 
   lg:flex lg:h-screen lg:items-center">
     <div className="mx-auto max-w-3xl text-center">
       <h1
-        className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+        className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl leading-relaxed pb-2"
       >
         Budget Tracker
 
